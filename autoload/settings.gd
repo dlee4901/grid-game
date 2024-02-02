e652b20ebe0b9e18489260025b0d7061c3bf8adf
dlee4901 <daniel.lee4901@gmail.com>
2024-02-02 14:45:26 -0600

    move to settings

--- autoload/settings.gd
+++ autoload/settings.gd
@@ -4,10 +4,20 @@ var screen_size = Vector2i(1920, 1080)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_tree().root.get_viewport().set_size(screen_size)
-	get_tree().root.get_viewport().set_content_scale_mode(1) 	# stretch_mode = canvas_items
-	get_tree().root.get_viewport().set_content_scale_aspect(1) 	# stretch_aspect = keep
+	init_window()
+	init_inputs()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func init_window():
+	get_tree().root.get_viewport().set_size(screen_size)
+	get_tree().root.get_viewport().set_content_scale_mode(1) 	# stretch_mode = canvas_items
+	get_tree().root.get_viewport().set_content_scale_aspect(1) 	# stretch_aspect = keep
+
+func init_inputs():
+	var mouse_button = InputEventMouseButton.new()
+	mouse_button.button_index = 1
+	InputMap.add_action("mouse_left_click")
+	InputMap.action_add_event("mouse_left_click", mouse_button)
--- main.gd
+++ main.gd
@@ -14,7 +14,6 @@ var grid
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	load_tree()
-	set_inputs()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -25,9 +24,3 @@ func load_tree():
 	game_state = Util.load_tree_script(self, GameState)
 	camera = Util.load_tree_scene(self, Camera)
 	grid = Util.load_tree_scene(self, Grid)
-
-func set_inputs():
-	var mouse_button = InputEventMouseButton.new()
-	mouse_button.button_index = 1
-	InputMap.add_action("mouse_left_click")
-	InputMap.action_add_event("mouse_left_click", mouse_button)
